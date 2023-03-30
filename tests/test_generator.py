c5b8a225f45ff9c4f66ca075476cd22ea7af6576
Polina Andreeva <polinaspb@ya.ru>
2023-03-30 16:53:36 +0300

    right test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,10 +1,21 @@
 from core.generator import Main
 
+
 def test_init_n_params():
     params = {
-        'N': 2,
+        "N": 100,
     }
 
-    gen = Main(N=params['N'])
+    gen = Main(
+        N=params["N"],
+        max_d=500,
+        L=3,
+        etta=0.2,
+        ro=1.1,
+        mu=0.9,
+        sigma_init=1,
+        sigma_every=1,
+        dim=32,
+    )
 
-    assert gen.N == params['N']
+    assert gen.N == params["N"]
